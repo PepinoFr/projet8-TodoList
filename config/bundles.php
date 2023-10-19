21c7307adf565063d1928e4d114d277fb10dad5a
IC-Mathieu <mathieu@inovea-conseil.com>
2023-10-19 21:44:36 +0200

    version 4.4

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
